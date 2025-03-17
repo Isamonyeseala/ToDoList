ca3c6ee9c7ce07d47282614230c598c83e61c228
Your Name <221768963@edu.vut.ac.za>
2025-03-17 14:41:41 +0200

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -2,13 +2,9 @@ const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
   description: { type: String, required: true },
+  priority: { type: String, required: true },
   completed: { type: Boolean, default: false },
-  priority: {
-    type: String,
-    enum: ["Do Now", "Schedule", "Delegate", "Eliminate"],
-    required: true,
-  },
+  userId: { type: String, required: true }, // Change from ObjectId to String
 });
 
-const Task = mongoose.model("Task", taskSchema);
-module.exports = Task;
+module.exports = mongoose.model("Task", taskSchema);
